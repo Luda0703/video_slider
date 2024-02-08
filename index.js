ad4078c7b3abab3cdf56f207a6c3ee6ebfa7be46
Nikitenko Ludmila <117203301+Luda0703@users.noreply.github.com>
2024-02-08 17:23:10 +0200

    поп-апи

--- index.js
+++ index.js
@@ -6,11 +6,15 @@ const swiper = new Swiper(".swiper", {
   loop: true,
   slidesPerView: 4,
   slidesPerGroup: 1,
-  spaceBetween: 20,
+  // spaceBetween: 20,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
   pagination: {
     el: ".swiper-pagination",
     clickable: true,
-    dynamicBullets: true,
+    // dynamicBullets: true,
   },
   keyboard: {
     enabled: true,
@@ -21,9 +25,24 @@ const swiper = new Swiper(".swiper", {
     sensitivity: 1,
     eventsTarget: ".video-slider",
   },
+  breakpoints: {
+    320: {
+      slidesPerView: 1,
+    },
+    480: {
+      slidesPerView: 2,
+    },
+    780: {
+      slidesPerView: 3,
+    },
+    992: {
+      slidesPerView: 4,
+    },
+  },
   speed: 800,
   grabCursor: true,
-  simulateTouch: true,
+  zoom: true,
+ 
 });
 
 async function getVideoInfo(videoId) {
@@ -46,9 +65,10 @@ document.addEventListener("DOMContentLoaded", async function () {
   for (let i = 0; i < 8; i++) {
     const slide = document.createElement("div");
     slide.classList.add("swiper-slide");
+    slide.setAttribute("data-video", `https://player.vimeo.com/video/${videoId}`);
 
     const iframe = document.createElement("iframe");
-    iframe.src = "https://player.vimeo.com/video/824804225";
+    iframe.src = `https://player.vimeo.com/video/${videoId}`;
     iframe.width = "400";
     iframe.height = "400";
     iframe.allowFullscreen = true;
@@ -58,4 +78,33 @@ document.addEventListener("DOMContentLoaded", async function () {
 
     swiper.appendSlide(slide);
   }
+
+  const popupOverlay = document.querySelector('.popup-overlay');
+  const videoContainer = document.querySelector('.video-container');
+  const closePopupBtn = document.querySelector('.close-popup');
+
+  swiper.on('click', '.swiper-slide', function() {
+    const videoUrl = this.getAttribute('data-video');
+    const videoIframe = `<iframe src="${videoUrl}?autoplay=1" frameborder="0" allowfullscreen></iframe>`;
+    videoContainer.innerHTML = videoIframe;
+    popupOverlay.style.display = 'flex';
+  });
+
+  closePopupBtn.addEventListener('click', function() {
+    videoContainer.innerHTML = '';
+    popupOverlay.style.display = 'none';
+  });
+
+  // Добавляем обработчик события для клика по текущему слайду в поп-апе
+  swiper.on('click', '.swiper-slide', function() {
+    const videoUrl = this.getAttribute('data-video');
+    const videoIframe = `<iframe src="${videoUrl}?autoplay=1" frameborder="0" allowfullscreen></iframe>`;
+    videoContainer.innerHTML = videoIframe;
+  });
 });
+
+
+
+
+
+
